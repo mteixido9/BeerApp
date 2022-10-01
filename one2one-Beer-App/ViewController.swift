492e578de634753f257c86b6c92c535df72e71b8
Marc Teixidó <mteixido9@gmail.com>
2022-10-01 12:00:30 +0200

    Initial Commit

---
+++ one2one-Beer-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  one2one-Beer-App
+//
+//  Created by Marc  Teixidó Carrera on 1/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ one2one-Beer-AppUITests/one2one_Beer_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  one2one_Beer_AppUITestsLaunchTests.swift
+//  one2one-Beer-AppUITests
+//
+//  Created by Marc  Teixidó Carrera on 1/10/22.
+//
+
+import XCTest
+
+class one2one_Beer_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
